88b22faced49e46d03bbd922054e9d833fa1815e
Debojeet Personal <debojeetkarmakar2004@outlook.com>
2026-02-27 20:38:03 +0530

    Ready for Vercel deployment i guess :)

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import composeRoute from "./api/compose/route";
 import featuresRoute from "./api/admin/features/route";
 import frameworkRoute from "./api/admin/frameworks/route";
 import linkRoute from "./api/ff-relation/route";
+import cors from "cors";
 
 
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", composeRoute);
 app.use("/api/features", featuresRoute);
